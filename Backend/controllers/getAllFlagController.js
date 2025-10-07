b1e6a93833e151b578cd831a4122b587277f0c15
Prasthegeek <sharmaprashant9763@gmail.com>
2025-10-07 17:16:39 +0530

    made responses a bit more readable and simpler

--- Backend/controllers/getAllFlagController.js
+++ Backend/controllers/getAllFlagController.js
@@ -5,10 +5,10 @@ export const getAllFlagController= async(req, res ) =>{
         const result = await getAllFlag() 
         // console.log(result) //return array of flags
         if(result.length == 0) return res.status(200).json({message:'there are no flag in the table'})
-        else return res.status(200).json({flag:result}) //show full array
+        else return res.status(200).json({flags:result}) //show full array
     }
     catch(e){
-        console.log("error while fetching all the flags from table " , e.stack) ;
+        console.log("error while fetching flags from table " , e.stack) ;
         return res.status(500).json({error : 'Backend issue , failed to fetch all the flags from the table'})
     }
 }
