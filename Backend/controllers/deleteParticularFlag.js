5730d64017b1900066000456ebb156e3162f9d65
Prasthegeek <sharmaprashant9763@gmail.com>
2025-10-13 03:37:04 +0530

    added redis cache for -> deleting /deactivating a flag

--- Backend/controllers/deleteParticularFlag.js
+++ Backend/controllers/deleteParticularFlag.js
@@ -1,15 +1,21 @@
+import redisClient from "../config/redisConfig.js";
 import { deleteFlag } from "../model/featureFlagsModel.js";
 
 export const deleteParticularFlag = async(req, res) =>{
     const id = parseInt(req.params?.id , 10 )//convert to integer in case string is received
-    if(!id || id=='undefined'){
-        return res.status(400).json({message:'id is required'}) 
+    if (isNaN(id)) {
+        return res.status(400).json({ message: 'Invalid or missing ID' })
     }
     try{
+        // Soft delete: update is_active = false(since, analytics me use)
         const result = await deleteFlag(id);
         if(!result){
-            return res.status(400).json({message:'No flag with id -> {$id} is found'})
+            return res.status(400).json({message:`No flag with id -> {$id} is found or flag was already deactivated`})
         }
+        //delete stored instance of this flag from redis
+        const cacheKey = `flag:${id}`
+        const cacheRes = await redisClient.del(cacheKey) //throws no error even if not present in redis
+        //don't cache again in case of delete (my rule)
         return res.status(200).json({message:`flag with id->${id} has been deactivated`, flag:result})
     }catch(err){
         console.log(err.stack)
