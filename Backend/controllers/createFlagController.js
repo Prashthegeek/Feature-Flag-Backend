3ab46b057e0ec7edd49b0bb575aa72711d8a65ac
Prasthegeek <sharmaprashant9763@gmail.com>
2025-10-12 23:09:46 +0530

    made rolloutPercentage to rollout_percentage to avoid confusion in api requests

--- Backend/controllers/createFlagController.js
+++ Backend/controllers/createFlagController.js
@@ -2,18 +2,18 @@ import { insertFlag } from "../model/featureFlagsModel.js"
 
 export const createFlagController= async(req, res) =>{
     try{
-        const {name , description , rolloutPercentage} = req.body 
+        const {name , description , rollout_percentage} = req.body 
         // console.log(req.body)
-        if(!name || rolloutPercentage === undefined) {
-            return res.status(400).json({error: 'both name and rollout percentage fields are compulsorily required'})
+        if(!name || rollout_percentage === undefined) {
+            return res.status(400).json({error: 'both name and rollout_percentage fields are compulsorily required'})
         } 
 
-        const rollout = Number(rolloutPercentage);  //in case someone gives rolloutPercentage as string in the req body
+        const rollout = Number(rollout_percentage);  //in case someone gives rolloutPercentage as string in the req body
             if (isNaN(rollout)) {
-            return res.status(400).json({ error: "rolloutPercentage must be numeric" });
+            return res.status(400).json({ error: "rollout_percentage must be numeric" });
         }
 
-        const newFlag = await insertFlag({name , description , rolloutPercentage:rollout}) //returns the flag or the tuple
+        const newFlag = await insertFlag({name , description , rollout_percentage:rollout}) //returns the flag or the tuple
         res.status(200).json({message:'flag creation succesfull' , flag:newFlag})
     }
     catch(err){
